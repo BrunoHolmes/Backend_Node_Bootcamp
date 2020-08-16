7efa7fa4567adba492dae43e16993dbd8e0bb966
BrunoHolmes <52051840+BrunoHolmes@users.noreply.github.com>
2020-08-16 18:43:41 -0300

    Aplicação funcional

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { response } = require('express');
+const { uuid } = require('uuidv4');
 
 const app = express();
 
@@ -22,48 +23,62 @@ app.use(express.json());
  * Request Body: Conteúdo na hora de criar ou editar um recurso através do JSON
  */
 
+const projects = [];
+
 app.get('/projects', (request, response) => {
-    const { title, owner } = request.query;
+    const { title } = request.query;
 
-    console.log(title);
-    console.log(owner);
+    const results = title
+        ? projects.filter(project => project.title.includes(title))
+        : projects;
 
-
-    return response.json([
-        'Projeto 1',
-        'Projeto 2'
-    ]);
+    return response.json(results);
 });
 
 app.post('/projects', (req, res) => {
     const { title, owner } = req.body;
-    console.log(title);
-    console.log(owner);
 
+    const project = { id: uuid(), title, owner };
 
-    return res.json([
-        'Projeto 1',
-        'Projeto 2',
-        'Projeto 3'
-    ])
+    projects.push(project);
+
+    return res.json(project)
 })
 
 app.put('/projects/:id', (req, res) => {
     const { id } = req.params;
-    console.log(id);
 
-    return res.json([
-        'Projeto 4',
-        'Projeto 2',
-        'Projeto 3'
-    ])
+    const { title, owner } = req.body;
+
+    const projectIndex = projects.findIndex(project => project.id === id);
+
+    if (projectIndex < 0) {
+        return res.status(400).json({ error: "project not found" });
+    }
+
+    const project = {
+        id,
+        title,
+        owner
+    };
+
+    projects[projectIndex] = project;
+
+    return res.json(project);
 })
 
 app.delete('/projects/:id', (req, res) => {
-    return res.json([
-        'Projeto 4',
-        'Projeto 3'
-    ])
+    const { id } = req.params;
+
+    const projectIndex = projects.findIndex(project => project.id === id);
+
+    if (projectIndex < 0) {
+        return res.status(400).json({ error: "project not found" });
+    }
+
+    project.splice(projectIndex, 1);
+
+    return res.status(204).json.send()
 })
 
 app.listen(3333, () => {

0d0950da26d90a20651c8244de851456c24fba73
BrunoHolmes <52051840+BrunoHolmes@users.noreply.github.com>
2020-08-16 17:02:24 -0300

    Métodos HTTP

--- src/index.js
+++ src/index.js
@@ -1,10 +1,46 @@
 const express = require('express');
+const { response } = require('express');
 
 const app = express();
 
-app.get('/', (request, response) => {
-    return response.json({ message: 'Hello GoStack' });
+/**
+ *  Métodos HTTP:
+ * 
+ * GET: Buscar informações do back-end
+ * POST: Criar uma informação no back-end
+ * PUT/PATCH: Alterar uma informação no back-end
+ * DELETE: Deletar uma informação no back-end
+ */
+
+app.get('/projects', (request, response) => {
+    return response.json([
+        'Projeto 1',
+        'Projeto 2'
+    ]);
 });
+
+app.post('/projects', (req, res) => {
+    return response.json([
+        'Projeto 1',
+        'Projeto 2',
+        'Projeto 3'
+    ])
+})
+
+app.put('/projects/:id', (req, res) => {
+    return response.json([
+        'Projeto 4',
+        'Projeto 2',
+        'Projeto 3'
+    ])
+})
+
+app.delete('/projects/:id', (req, res) => {
+    return response.json([
+        'Projeto 4',
+        'Projeto 3'
+    ])
+})
 
 app.listen(3333, () => {
     console.log('🚀 Back-end started!');
